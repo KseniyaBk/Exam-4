bf2b722cd7c2b0ef2be9ddbf4a4972c8d00e77b7
Kseniya <tellur312@gmail.com>
2017-03-07 15:21:28 +0600

    added new directory and files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Ксения on 25.02.2017.
+ */
+$('#reg-form').validate({
+    rules: {
+        name:{
+            minlength: 5
+        }
+    }
+});
+$('#phone').mask("(999) 999-9999");
+
+
+$('#popup-open').click(function(){
+    $('.popup-wrap').show('slow')
+});
+$('#popup-wrap').click(function(){
+    $('.popup-wrap').hide('slow')
+});
